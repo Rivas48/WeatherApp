4a4f2ba9032b05596d5d0bffd1079faec28e46f8
Bryant Rivas <bryantrivas1127@yahoo.com>
2018-11-04 14:18:59 -0500

    Updates

---
+++ precache-manifest.fe471d5eb90f861d40eea69317f4ac43.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c84f61e8577452f9008097a3349dbac5",
+    "url": "/WeatherApp/static/media/fog.c84f61e8.jpg"
+  },
+  {
+    "revision": "ed38f88929677b50fd11b14506596ecb",
+    "url": "/WeatherApp/static/media/cloudy.ed38f889.jpeg"
+  },
+  {
+    "revision": "a0bcfe45b310c6972b55517332775466",
+    "url": "/WeatherApp/static/media/Rain.a0bcfe45.jpg"
+  },
+  {
+    "revision": "a65dfe9f73dd55aa5b62",
+    "url": "/WeatherApp/static/js/runtime~main.a65dfe9f.js"
+  },
+  {
+    "revision": "a31380d2df98d8399ffc",
+    "url": "/WeatherApp/static/js/main.a31380d2.chunk.js"
+  },
+  {
+    "revision": "37f90d57916b981a0157",
+    "url": "/WeatherApp/static/js/1.37f90d57.chunk.js"
+  },
+  {
+    "revision": "a31380d2df98d8399ffc",
+    "url": "/WeatherApp/static/css/main.1f5ff34f.chunk.css"
+  },
+  {
+    "revision": "2f58b20bc77512e21fd872fc646e1f18",
+    "url": "/WeatherApp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/weather-app/precache-manifest.cae33f78b0bf31ffb1bff2269d2d5a6d.js"
+  "/WeatherApp/precache-manifest.fe471d5eb90f861d40eea69317f4ac43.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/weather-app/index.html", {
+workbox.routing.registerNavigationRoute("/WeatherApp/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
